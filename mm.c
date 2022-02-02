c2b6aba204a35eb3db084a9090e8de468b85e634
Weilin-W <wpw5138@psu.edu>
2022-02-01 21:27:10 -0500

    V0.1.7(Find fit, Place)

--- mm.c
+++ mm.c
@@ -150,7 +150,38 @@ static void *coalesce(void* bp){
     }
     return (bp); //return block pointer
 }
+/*
+ * Find fit function
+ */
+static void *find_fit(size_t asize){
+    //First fit search
+    void *bp;
 
+    for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)){
+        if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))){
+            return bp;
+        }
+    }
+    return NULL; //No fit
+}
+
+/*
+ * Place function
+ */
+static void place(void *bp, size_t asize){
+    size_t csize = GET_SIZE(HDRP(bp));
+
+    if((csize - asize) >= (2*DSIZE)){
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        bp = NEXT_BLKP(bp);
+        PUT(HDRP(bp), PACK(csize - asize, 0));
+        PUT(FTRP(bp), PACK(csize - asize, 0));
+    }else{
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
+}
 
 
 /*
